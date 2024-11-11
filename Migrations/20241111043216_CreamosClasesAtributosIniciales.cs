d53cf1099e053dcd4b72c58ba2cee312fcc8f55e
julianvazquez24 <julivazquez248@gmail.com>
2024-11-11 01:33:03 -0300

    Actuallizo BDD

---
+++ Migrations/20241111043216_CreamosClasesAtributosIniciales.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FormulaUnoObligatorio.Migrations
+{
+    /// <inheritdoc />
+    public partial class CreamosClasesAtributosIniciales : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
